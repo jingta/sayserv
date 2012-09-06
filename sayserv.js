8d4dabda698c2c5b3e1d35a7d620f79a7b23bbb2
Jean-Charles Sisk <jasisk@gmail.com>
2012-09-06 15:08:24 -0400

    Fixing a bad reference, trailing whitespace, and adding reverse lookup

--- sayserv.js
+++ sayserv.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 var child;
 var PORT = 1337;
 
-var msg = "Say Server: /voice/message or /message\n\n" + 
+var msg = "Say Server: /voice/message or /message\n\n" +
     "Agnes               en_US    # Isn't it nice to have a computer that will talk to you?\n" +
     "Albert              en_US    #  I have a frog in my throat. No, I mean a real frog!\n" +
     "Alex                en_US    # Most people recognize me by my voice.\n" +
@@ -34,6 +34,7 @@ var sayservs = {};
 
 // import the module
 var mdns = require('mdns');
+var dns = require('dns');
 
 // advertise a http server on port 4321
 var ad = mdns.createAdvertisement(mdns.tcp('sayserv'), 4321);
@@ -42,16 +43,16 @@ ad.start();
 // watch all http servers
 var browser = mdns.createBrowser(mdns.tcp('sayserv'));
 browser.on('serviceUp', function(service) {
-	//	console.log("service up: ", service);
-	var broadcast_count = sayservs[service.name] ? sayservs[service.name].broadcast_count + 1 : 1;
-	sayservs[service.name] = service;
-	sayservs[service.name].broadcast_count = broadcast_count;
-	console.log("service up: %s [count:%s]", service.name, sayservs[service.name].broadcast_count);
+  //  console.log("service up: ", service);
+  var broadcast_count = sayservs[service.name] ? ( sayservs[service.name].broadcast_count || 0 ) + 1 : 1;
+  sayservs[service.name] = service;
+  sayservs[service.name].broadcast_count = broadcast_count;
+  console.log("service up: %s [count:%s]", service.name, sayservs[service.name].broadcast_count);
     });
 browser.on('serviceDown', function(service) {
-	sayservs[service.name].broadcast_count = sayservs[service.name].broadcast_count - 1;
-	console.log("service down: %s [count:%s]", service.name, sayservs[service.name].broadcast_count);
-	if (sayservs[service.name].broadcast_count < 1) delete sayservs[service.name];
+  sayservs[service.name].broadcast_count = sayservs[service.name].broadcast_count - 1;
+  console.log("service down: %s [count:%s]", service.name, sayservs[service.name].broadcast_count);
+  if (sayservs[service.name].broadcast_count < 1) delete sayservs[service.name];
     });
 browser.start();
 
@@ -63,7 +64,7 @@ var activeServices = function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("Services:\n\n");
     for(var key in sayservs) {
-	response.write(sayservs[key].host + ':' + PORT + '\n');
+  response.write(sayservs[key].host + ':' + PORT + '\n');
     }
     response.end();
 };
@@ -73,52 +74,61 @@ var broadcastMessage = function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("Broadcasting to Services:\n\n");
     for(var key in sayservs) {
-	var options = {
-	    host: sayservs[key].host,
-	    port: PORT,
-	    path: request.url.replace('/broadcast/', '/'),
-	    method: 'GET'
-	};
-	
-	var req = http.request(options, function(res) {
-		res.setEncoding('utf8');
-		res.on('end', function (chunk) {
-			console.log('Message sent to ' + sayservs[key].host );
-		    });
-	    });
-	req.on('error', function(e) {
-		console.log('Problem with request to ' + sayservs[key].host + ': ' + e.message);
-	    });
-	// finish request
-	req.end();
-	
-	response.write(sayservs[key].host + ':' + PORT + '\n');
+  var options = {
+      host: sayservs[key].host,
+      port: PORT,
+      path: request.url.replace('/broadcast/', '/'),
+      method: 'GET'
+  };
+  
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('end', function (chunk) {
+      console.log('Message sent to ' + sayservs[key].host );
+        });
+      });
+  req.on('error', function(e) {
+    console.log('Problem with request to ' + sayservs[key].host + ': ' + e.message);
+      });
+  // finish request
+  req.end();
+  
+  response.write(sayservs[key].host + ':' + PORT + '\n');
     }
     response.end();
 };
-	
+  
 var http = require('http');
 http.createServer(function (request, response) {
-	// ignore favicon requests
-	if (request.url === "/favicon.ico") return;
-	
-	if (request.url === "/browser") return activeServices(request, response);
-	
-	if (request.url.match(/^\/broadcast\//)) return broadcastMessage(request, response);
-	
-	// send voices / instructions
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end(msg);	
-	
-	var segments = request.url.split('/');
-	var message = segments[segments.length - 1];
-	var voice = segments[segments.length - 2];
-	
-	message = decodeURIComponent(message).replace(/\W/g,' ');
-	voice = decodeURIComponent(voice).replace(/\W/g,' ');
-	var cmd = "say " + (voice ? "-v " + voice + ' ' : '')  + message;
-	child = exec(cmd);
-	console.log('cmd: ' + cmd);
+  // ignore favicon requests
+  if (request.url === "/favicon.ico") return;
+  
+  if (request.url === "/browser") return activeServices(request, response);
+  
+  if (request.url.match(/^\/broadcast\//)) return broadcastMessage(request, response);
+  
+  // send voices / instructions
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end(msg);
+  
+  var segments = request.url.split('/');
+  var message = segments[segments.length - 1];
+  var voice = segments[segments.length - 2];
+  var host = request.connection.remoteAddress;
+  
+  message = decodeURIComponent(message).replace(/\W/g,' ');
+  voice = decodeURIComponent(voice).replace(/\W/g,' ');
+  var cmd = "say " + (voice ? "-v " + voice + ' ' : '')  + message;
+  child = exec(cmd);
+
+  dns.reverse(host, function(err, domains) {
+    if( err == null ) {
+      console.log('[' + domains + '] cmd: ' + cmd);
+    } else {
+      console.log('[' + host + '] cmd: ' + cmd);
+    }
+  });
+
     }).listen(PORT, '0.0.0.0');
 console.log('Server running at http://0.0.0.0:' + PORT);
 
